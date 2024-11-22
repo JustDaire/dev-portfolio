7761011ed4b860fa68ead070f143afbf87a74f70
Daire <daire@protonmail.com>
2024-11-22 14:56:40 +0000

    Updated header with navbar and social links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,10 +5,37 @@ import { DiCssdeck } from 'react-icons/di';
 
 import { Container, Div1, Div2, Div3, NavLink, SocialIcons } from './HeaderStyles';
 
+import SocialDetails from './SocialDetails';
+
 const Header = () =>  (
-  <div>
-    Header
-  </div>
+  <Container>
+    <Div1>
+      <Link href="/">
+        <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: "20px" }}>
+          <DiCssdeck size="3rem" />
+          <span>Portfolio</span>
+        </a>
+      </Link>
+    </Div1>
+    <Div2>
+      <li>
+        <Link href="#projects">
+          <NavLink>Projects</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#technologies">
+          <NavLink>Technologies</NavLink>
+        </Link>
+      </li>
+      <li>
+        <Link href="#about">
+          <NavLink>About</NavLink>
+        </Link>
+      </li>
+    </Div2>
+    <SocialDetails />
+  </Container>
 );
 
 export default Header;
