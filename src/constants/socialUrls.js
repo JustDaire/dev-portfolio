79a156cc40122dd9479a52139884b89b01806e20
Daire <daire@protonmail.com>
2024-11-22 14:55:52 +0000

    Added social links for routing

---
+++ src/constants/socialUrls.js
@@ -0,0 +1,8 @@
+const socialUrls = {
+    github: "https://github.com/JustDaire",
+    linkedIn: "https://www.linkedin.com/in/dairehardesty/",
+    twitter: "https://twitter.com/JustDaire",
+    youtube: "https://www.youtube.com/JustDaire",
+  };
+  
+  module.exports = { socialUrls };
